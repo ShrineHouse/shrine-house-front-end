05b0f13e14f99032c2bfdcba8738d8d4353abf33
girbaudbarthels <girbaud@rvised.com>
2022-05-31 19:46:26 +0200

    added autosave on user signup

--- src/signup/stepThree.tsx
+++ src/signup/stepThree.tsx
@@ -1,17 +1,18 @@
+import { Center, CircularProgress } from '@chakra-ui/react';
 import Moralis from 'moralis/types';
 import * as React from 'react';
 import { MoralisObjectSaveData, useMoralis, useNewMoralisObject } from 'react-moralis';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../components/logo';
 import { useGetFetchQuery } from '../helpers/state';
 import { DbUser, upUser, upUserSocials } from '../interfaces/users';
 
 
 const StepThree = (props: { data: upUser, dataSocials: upUserSocials }) => {
-    const initImg = 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png?20170328184010';
-    const [image, setImage] = React.useState(initImg)
-    const { isSaving, error, save, } = useNewMoralisObject('_Users');
     const { user } = useMoralis();
+    const [isSaving, setSaving] = React.useState(false)
     console.log(props.data, props.dataSocials)
+    const navigate = useNavigate();
 
     async function updateUser() {
         if (user !== null) {
@@ -51,9 +52,17 @@ const StepThree = (props: { data: upUser, dataSocials: upUserSocials }) => {
             user.set('wallet', dbUser.wallet)
 
             user.save()
+
+            navigate('/')
+
         }
 
     }
+
+    React.useEffect(() => {
+        updateUser()
+    }, [])
+
     return (
 
         <div className='flex flex-col items-center justify-center min-h-screen gap-5 p-10 px-20'>
@@ -61,7 +70,7 @@ const StepThree = (props: { data: upUser, dataSocials: upUserSocials }) => {
                 <Logo />
             </div>
             <div className='titleText'>Registration complete</div>
-            <button onClick={updateUser}>save</button>
+            <Center><CircularProgress isIndeterminate /></Center>
         </div>
 
     )
