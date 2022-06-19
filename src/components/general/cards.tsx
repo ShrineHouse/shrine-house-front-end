8e3c5386e94baad4c6529eb9595b2e8bff452f85
girbaudbarthels <girbaud@rvised.com>
2022-06-19 19:14:19 +0200

    fixed small card aspect ratio

--- src/components/general/cards.tsx
+++ src/components/general/cards.tsx
@@ -11,10 +11,10 @@ export const BigArtistCard = (props: { url: string }) => {
 
 export const SmallArtistCard = (props: { url: string, artistName: string, verified: boolean }) => {
     return (
-        <div className='w-full h-full flex flex-col gap-2'>
+        <div className='w-full flex flex-col gap-2'>
 
-            <div className='w-full h-56 bg-white borderRadiusComponents shadow bigCard'>
-                <img src={props.url} className='relative h-full w-full object-cover borderRadiusComponents' />
+            <div className='w-full  bg-white borderRadiusComponents shadow bigCard '>
+                <img src={props.url} className='relative h-full w-full object-cover borderRadiusComponents smallCard' />
             </div>
 
             <div className='flex flex-row justify-between'> <p>{props.artistName}</p> {props.verified && <div className="primaryColor"><AiFillCheckCircle size={24} /></div>} </div>
